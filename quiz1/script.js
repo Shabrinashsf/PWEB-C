f586f5fb410dd5ba4b53b612a23c7d2cea21e868
Shabrina Amalia Safaana <shabrinaamalia860@gmail.com>
2025-09-22 03:07:37 +0700

    add image folder and create home page

---
+++ quiz1/script.js
@@ -0,0 +1,47 @@
+// Navbar
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+// Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("active");
+});
+
+// Carousel Image Slider
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlides = slides.querySelector("[data-active]");
+
+    let newIndex = [...slides.children].indexOf(activeSlides) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlides.dataset.active;
+  });
+});
+
+// Smooth Load
+window.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("main").classList.add("show");
+  document.querySelector("footer").classList.add("show");
+});
